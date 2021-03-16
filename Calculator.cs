0bf6dce28efe1104d5fd7ed435321d7908743970
putrifelda <79987392+putrifelda@users.noreply.github.com>
2021-03-16 07:35:02 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,24 @@
 class Calculator
 {
-	static void Main(string[] args)
+	static void Main (string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} + {1} = {2}", a, b, Pengurangan(a, b));
+	
+		Console.WriteLine("\nTekan Sembarang Key untuk keluar");
+		Console.Readkey();
+	}
+	
+	static int Penambahan(int a,int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a,int b)
+	{
+		return a - b;
 	}
 }
